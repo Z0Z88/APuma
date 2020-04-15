0beec81e37024bf7d19dd4fd8ad18d97e8b73043
Péter Pivarcsik <peter.pivarcsik@gmail.com>
2020-04-16 00:27:48 +0200

    Az alap logika commentelése TODO-k formájában.

--- src/main/java/APuMa.java
+++ src/main/java/APuMa.java
@@ -1,2 +1,9 @@
 public class APuMa {
+    public static void main(String[] args) {
+        // TODO: A cég logo meghívása
+        // TODO: A játék logo meghívása
+        // TODO: Intro meghívása
+        // TODO: Menü meghívása
+        // TODO: Esetleg valami kilépés után?!?
+    }
 }
--- src/test/java/APuMaTest.java
+++ src/test/java/APuMaTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class APuMaTest {
+    @Test
+    public void mainTest () {
+        // TODO Itt minden publikus method-hoz készülnie kell egy tesztnek
+        Assertions.assertTrue(true);
+    }
 }
